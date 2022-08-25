ba29734b6589194544b0daf0d865fa8e34b3c3a0
joshua <joshuadarecullen@protonmail.com>
2022-08-25 17:02:56 +0100

    add player draw func

--- base/player.py
+++ base/player.py
@@ -5,5 +5,6 @@ from base import *
 class Player(Agent):
     def __init__(self, x, y, theta):
         super().__init__(x,y,theta) # super is the parent of Player: Agent, we have inherited x, y and theta as attributes for use in this class.
-    def player_draw(self):
+
+    def player_drawing(self):
         pass
