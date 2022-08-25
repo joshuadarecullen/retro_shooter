2818163a4ca3e35f920139ecc57aefd71f4b68ee
Joshua <72976308+joshuadarecullen@users.noreply.github.com>
2022-08-25 16:29:18 +0100

    Delete Player.py
    change name of file

--- base/Player.py
+++
@@ -1,7 +0,0 @@
-from .base import *
-
-# inherit from the abstract class Agent, check it out
-# allows for less rep
-class Player(Agent):
-    def __init__(self, x, y, theta):
-        super.__init__(x,y,theta) # super is the parent of Player: Agent, we have inherited x, y and theta as attributes for use in this class.
