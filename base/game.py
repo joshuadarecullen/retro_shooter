7da53c7bc33c6b44e315a3d18f2e287043ea1ffa
joshua <joshuadarecullen@protonmail.com>
2022-08-29 20:17:16 +0100

    added scenes

--- base/game.py
+++ base/game.py
@@ -14,10 +14,11 @@ class Game:
         # initialise pygame and application
         self.initialise_game()
         flags = RESIZABLE
-        Game.screen = pygame.display.set_mode((width, height), flags)
+        self.rect = Rect(0, 0, 640, 240)
+        Game.screen = pygame.display.set_mode(self.rect.size, flags)
 
         # UI start screen initialisation
-        self.background = pygame.image.load('./sprites/backgrounds/menus/StartBackground.jpg') # background
+        self.background = None
         # window variables
         self.width = width
         self.height = height
@@ -37,6 +38,58 @@ class Game:
         self.characters = self.get_characters()
         self.selected_character = None
 
+        self.shortcuts = {
+            (K_x, KMOD_LMETA): 'print("cmd+X")',
+            (K_x, KMOD_LALT): 'print("alt+X")',
+            (K_x, KMOD_LCTRL): 'print("ctrl+X")',
+            (K_x, KMOD_LMETA + KMOD_LSHIFT): 'print("cmd+shift+X")',
+            (K_x, KMOD_LMETA + KMOD_LALT): 'print("cmd+alt+X")',
+            (K_x, KMOD_LMETA + KMOD_LALT + KMOD_LSHIFT): 'print("cmd+alt+shift+X")',
+            (K_f, KMOD_LMETA): 'self.toggle_fullscreen()',
+            (K_r, KMOD_LMETA): 'self.toggle_resizable()',
+            (K_g, KMOD_LMETA): 'self.toggle_frame()',
+        }
+
+    def get_scenes(self):
+
+        Scene(caption='Intro')
+        Text('Scene 0')
+        Text('Introduction screen the app')
+
+        Scene(bg=Color('yellow'), caption='Options')
+        Text('Scene 1')
+        Text('Option screen of the app')
+
+        Scene(bg=Color('green'), caption='Main')
+        Text('Scene 2')
+        Text('Main screen of the app')
+        Game.scene = App.scenes[0]
+
+    def do_shortcut(self, event):
+        """Find the the key/mod combination in the dictionary and execute the cmd."""
+        k = event.key
+        m = event.mod
+        if (k, m) in self.shortcuts:
+            exec(self.shortcuts[k, m])
+
+
+    def toggle_fullscreen(self):
+        """Toggle between full screen and windowed screen."""
+        self.flags ^= FULLSCREEN
+        pygame.display.set_mode((0, 0), self.flags)
+
+
+    def toggle_resizable(self):
+        """Toggle between resizable and fixed-size window."""
+        self.flags ^= RESIZABLE
+        pygame.display.set_mode(self.rect.size, self.flags)
+
+
+    def toggle_frame(self):
+        """Toggle between frame and noframe window."""
+        self.flags ^= NOFRAME
+        pygame.display.set_mode(self.rect.size, self.flags)
+
 
     # Here we initialise pygame and the window
     def initialise_game(self):
@@ -47,13 +100,13 @@ class Game:
 
 
     # redraw what the user will be seeing
-    def redraw_window(self, window, background):
+    def redraw_window(self):
 
         # set background
-        window.blit(background, (0,0))
+        self.background = pygame.image.load('./sprites/backgrounds/menus/StartBackground.jpg') # background
 
-        # redraw the player in new state
-        self.user_player.player_drawing(window)
+        # # redraw the player in new state
+        # self.user_player.player_drawing(window)
 
 
     def respawn_player(self, player):
@@ -85,8 +138,11 @@ class Game:
 
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     pass
+                if event.type == KEYDOWN:
+                    self.do_shortcut(event)
 
             Game.start_text.draw()
+            self.redraw_window()
             pygame.display.update()
 
 
@@ -95,6 +151,30 @@ class Game:
             # self.redraw_window(seld.window, self.background) # redraw with respect to new state of the game
 
         pygame.quit()
+
+
+# Creating different scenes for the game, each screen an object
+class Scene:
+
+    def __init__(self, *args, **kwargs):
+        # Append the new scene and make it the current scene
+        Game.scenes.append(self)
+        Game.scene = self
+        # Set the instance id and increment the class id
+        self.id = Scene.id
+        Scene.id += 1
+        self.nodes = []
+        self.bg = Scene.bg
+
+    def draw(self):
+        """Draw all objects in the scene."""
+        Game.screen.fill(self.bg)
+        for node in self.nodes:
+            node.draw()
+        pygame.display.flip()
+
+    def __str__(self):
+        return f'Scene {self.id}'
 
 
 # Text class
